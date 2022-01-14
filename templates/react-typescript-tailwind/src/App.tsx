36786d27d55dc742b9e86ec2561e135efdefe31d
kumard3 <kumardeepanshu157200@gmail.com>
2022-01-14 18:58:30 +0530

    new template added

---
+++ templates/react-typescript-tailwind/src/App.tsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="bg-black min-h-screen flex justify-center items-center">
+      <h1 className="text-white text-9xl">Hello World with react route dom</h1>
+    </div>
+  );
+}
+
+export default App;
