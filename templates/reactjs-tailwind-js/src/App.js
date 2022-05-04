928375e61875e23d3fff48afa9682009a9ce0417
kumard3 <kumardeepanshu157200@gmail.com>
2022-05-04 20:02:47 +0530

    react template updated

--- templates/reactjs-tailwind-js/src/App.js
+++ templates/reactjs-tailwind-js/src/App.js
@@ -1,25 +1,33 @@
-import { Route, Routes } from "react-router-dom";
-import NavComponent from "./components/NavComponent";
+import { Route, Routes } from 'react-router-dom'
+import NavComponent from './components/NavComponent'
 
 export const HomePage = () => {
   return (
-    <div className="bg-[#0F182B] text-white min-h-screen">
+    <div className="bg-black text-white">
       <h1>tailwind css starter </h1>
     </div>
-  );
-};
+  )
+}
 
+export const AboutPage = () => {
+  return (
+    <div className="bg-black text-white">
+      <h1>About</h1>
+    </div>
+  )
+}
 function App() {
   return (
-    <div className="">
+    <div className="bg-black text-white min-h-screen ">
       <NavComponent />
       <>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
         </Routes>
       </>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
