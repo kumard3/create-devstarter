bd5a8784d529a3b3e0ab96d1fe653d41b434f764
Kumar Deepanshu <62591080+kumard3@users.noreply.github.com>
2022-05-13 14:02:17 +0530

    Update tailwind.config.js

--- templates/nextjs-tailwind-typescript/tailwind.config.js
+++ templates/nextjs-tailwind-typescript/tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  purge: { enabled: true, content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],},
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {},
   },
